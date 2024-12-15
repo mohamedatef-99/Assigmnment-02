2effba500d2074e5cb9d388d34d52cbf8e462cfa
Mohamed Atef <mohammed.atef.saif@gmail.com>
2024-12-15 20:56:11 +0200

    String Format

--- Assigmnment 02/Program.cs
+++ Assigmnment 02/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            #region String Formating
+            //// String Formating
+            //// Equation : X + Y = 12
+
+            //int x = 5;
+            //int y = 7;
+
+            //// String Concatenation
+            //string Message = "Equation : " + x + " + " + y + " = " + (x + y);
+            //Console.WriteLine(Message);
+            //// String : Immutable => Once created, cannot be changed
+
+            //// String Formating
+            //string Message2 = string.Format("Equation : {0} + {1} = {2}", x, y, x + y);
+            //Console.WriteLine(Message2);
+
+            //// String Interpolation
+
+            //string Message3 = $"Equation : {x} + {y} = {x + y}";
+            //Console.WriteLine(Message3); 
+            #endregion
+
         }
     }
 }
